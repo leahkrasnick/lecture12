fda468b1322aa60f6e79f03c07a601a10d50e442
leahkras <leahkras@umich.edu>
2018-02-06 10:01:43 -0500

    added files

--- secrets.py
+++ secrets.py
@@ -1,2 +1,4 @@
 client_key = "l7aAC1zBGxDl0sqikWaNSCIMv"
 client_secret = "JNWgkJDZViyhTPNv6bQRaqfuEr0lruL9ShviVoF1uYKC1r8ciz"
+access_token = "	960878605218828288-esHKDVzkkbEflQjCHzTxhqliGIYL27Y"
+access_token_secret = "EqiNcIvzDLHklCjmFZMMBPRpAH8Rpw4lTFK5lKuNtOWZD"
